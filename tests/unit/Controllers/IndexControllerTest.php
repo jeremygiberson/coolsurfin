928eebd55277315bfd9291aa0964d917bf0c07a6
jeremygiberson@gmail.com <jeremygiberson@gmail.com>
2015-12-19 21:55:22 -0700

    added test for index controller

---
+++ tests/unit/Controllers/IndexControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Coolsurfin\Unit\Controllers;
+
+
+use JeremyGiberson\Coolsurfin\Controllers\IndexController;
+use Slim\Http\Environment;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class IndexControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_it_returns_200_response(){
+        $controller = new IndexController();
+        $environment = Environment::mock();
+        $request = Request::createFromEnvironment($environment);
+        $response = new Response;
+        /** @var Response $return */
+        $return = $controller($request, $response);
+        $this->assertInstanceOf(Response::class, $return);
+        $this->assertEquals(200, $return->getStatusCode());
+    }
+}
