a84abd04063484b2575949fa8e5f7a250655e265
Jeremy Giberson <jeremyg@webpt.com>
2015-12-26 16:22:24 -0700

    added a middleware that will be used to catch entitynotfound exceptions and render them as 404s

---
+++ src/Api/V1/Middleware/EntityNotFoundMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace JeremyGiberson\Coolsurfin\Api\V1\Middleware;
+
+
+use JeremyGiberson\Coolsurfin\Api\V1\Exception\EntityNotFound;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class EntityNotFoundMiddleware
+{
+    public function __invoke(Request $request, Response $response, callable $next){
+        try {
+            return $next($request, $response);
+        } catch (EntityNotFound $e) {
+            return $response->withStatus(404, $e->getMessage());
+        }
+    }
+}
